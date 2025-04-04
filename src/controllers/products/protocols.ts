95bd550924d9d3a82939aed0bf8999cc43c58320
ismael cezar <103972495+ismaelczar@users.noreply.github.com>
2025-04-03 21:28:00 -0300

    fix: adjustment to the structure

--- src/controllers/products/protocols.ts
+++
@@ -1,6 +0,0 @@
-import { Product } from '../../models/products';
-import { HttpResponse } from '../protocols';
-
-export interface IProductsController {
-  list(): Promise<HttpResponse<Product[]>>;
-}
