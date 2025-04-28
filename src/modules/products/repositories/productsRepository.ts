563aab8e2e628b7c8fba4c7e5df0c78336b7a2d0
ismael cezar <103972495+ismaelczar@users.noreply.github.com>
2025-04-28 19:23:51 -0300

    feat: add methods productsRepository

--- src/modules/products/repositories/productsRepository.ts
+++ src/modules/products/repositories/productsRepository.ts
@@ -1,15 +1,36 @@
+import { getRepository } from 'typeorm';
 import { Product } from '../infra/typeorm/entities/products';
 import { IProductsRepository } from './protocols';
 
 export class ProductsRepository implements IProductsRepository {
-  async getProducts(): Promise<Product[]> {
-    return [
-      {
-        name: 'Produto 1',
-        price: 1.99,
-        image: '',
-        description: 'descrição teste',
-      },
-    ];
+  //TODO: CRIAR UMA UNICA INSTANCIA DO REPOSITORIO.
+  async findAll(): Promise<Product[]> {
+    const ormRepository = getRepository(Product);
+    const products = await ormRepository.find();
+
+    return products;
+  }
+
+  async create(productData: Product): Promise<Product> {
+    const ormRepository = getRepository(Product);
+
+    const product = productData;
+
+    ormRepository.create(productData);
+
+    return product;
+  }
+
+  async remove(id: string): Promise<void> {
+    const ormRepository = getRepository(Product);
+    await ormRepository.delete(id);
+  }
+
+  async update(product: Partial<Product>): Promise<Product> {
+    const ormRepository = getRepository(Product);
+
+    const updatedProduct = await ormRepository.save(product);
+
+    return updatedProduct;
   }
 }
--- src/modules/products/repositories/protocols.ts
+++ src/modules/products/repositories/protocols.ts
@@ -3,6 +3,6 @@ import { Product } from '../infra/typeorm/entities/products';
 export interface IProductsRepository {
   findAll(): Promise<Product[]>;
   create(product: Product): Promise<Product>;
-  update(id: string, product: Partial<Product>): Promise<Product>;
+  update(product: Partial<Product>): Promise<Product>;
   remove(id: string): Promise<void>;
 }
