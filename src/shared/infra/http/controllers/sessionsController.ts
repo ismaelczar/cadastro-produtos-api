8c215b479602775069350eeba76ae7a06daf4b76
ismael cezar <103972495+ismaelczar@users.noreply.github.com>
2025-05-22 22:44:02 -0300

    refactor: sessions-controller

--- src/shared/infra/http/controllers/sessionsController.ts
+++ src/shared/infra/http/controllers/sessionsController.ts
@@ -1,12 +1,15 @@
-import { HttpResponse } from '../../../../config/httpResponse';
-import { User } from '../../../../modules/users/infra/typeorm/entities/user';
+import { AuthenticateUserResponse } from '@config/authenticated';
+import { HttpResponse } from '@config/httpResponse';
 
-import { AuthenticateUserService } from '../../../../modules/users/services/sessionsService';
+import { AuthenticateUserService } from '@modules/users/services/SessionsService';
 
 export class SessionsController {
   constructor(private readonly sessionsService: AuthenticateUserService) {}
 
-  async login(email: string, password: string): Promise<HttpResponse<User>> {
-    return this.sessionsService.login(email, password);
+  async login(
+    email: string,
+    password: string,
+  ): Promise<HttpResponse<AuthenticateUserResponse>> {
+    return this.sessionsService.execute(email, password);
   }
 }
