f84da07ed60f2464c3ff591899c35cdccbe16be2
ismael cezar <103972495+ismaelczar@users.noreply.github.com>
2025-05-30 19:14:12 -0300

    feat: refactor login-controller and login-use-case

--- src/modules/auth/application/useCases/login/LoginController.ts
+++ src/modules/auth/application/useCases/login/LoginController.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
-import { LoginUserCase } from '@modules/auth/application/useCases/login/LoginUseCase';
+import { LoginUseCase } from '@modules/auth/application/useCases/login/LoginUseCase';
 
 export class LoginController {
   async handle(req: Request, res: Response): Promise<Response> {
     const { email, password } = req.body;
 
-    const useCase = container.resolve(LoginUserCase);
+    const useCase = container.resolve(LoginUseCase);
     const result = await useCase.execute(email, password);
 
     return res.json(result);
--- src/modules/auth/application/useCases/login/LoginUseCase.ts
+++ src/modules/auth/application/useCases/login/LoginUseCase.ts
@@ -1,69 +1,41 @@
+import { injectable, inject } from 'tsyringe';
+import { IUserRepository } from '@modules/users/domain/repositories/IUserRepository';
 import { compare } from 'bcrypt';
 import { sign } from 'jsonwebtoken';
-import { IUserRepository } from '@modules/users/domain/repositories/IUserRepository';
-import { HttpResponse } from '../../../../../shared/responses/httpResponse';
-import { LoginResponse } from '@modules/auth/domain/dtos/LoginResponse';
-import { injectable, inject } from 'tsyringe';
 
 @injectable()
-export class LoginUserCase {
+export class LoginUseCase {
   constructor(
     @inject('UserRepository')
-    private readonly usersRepository: IUserRepository,
+    private userRepository: IUserRepository,
   ) {}
 
   async execute(
     email: string,
     password: string,
-  ): Promise<HttpResponse<LoginResponse>> {
-    try {
-      const userExist = await this.usersRepository.findByEmail(email);
+  ): Promise<{ token: string; refreshToken: string }> {
+    const userExist = await this.userRepository.findByEmail(email);
 
-      if (!userExist) {
-        return {
-          statusCode: 401,
-          body: 'Incorrect email/password comnination.',
-        };
-      }
-
-      const passwordMatched = await compare(password, userExist.password);
-
-      if (!passwordMatched) {
-        return {
-          statusCode: 401,
-          body: 'Incorrect email/password comnination.',
-        };
-      }
-
-      const accessToken = sign({}, 'a11113006a7272e0cfad95952e7e62f3', {
-        subject: userExist.id,
-        expiresIn: '15m',
-      });
-
-      const refreshToken = sign({}, 'a11113006a7272e0cfad95952e7e62f3', {
-        subject: userExist.id,
-        expiresIn: '7d',
-      });
-
-      const userAuthenticate = {
-        ...userExist,
-        accessToken,
-      };
-
-      delete userAuthenticate.password;
-
-      return {
-        statusCode: 200,
-        body: {
-          user: userAuthenticate,
-          refreshToken,
-        },
-      };
-    } catch (error) {
-      return {
-        statusCode: 500,
-        body: 'Internal server error',
-      };
+    if (!userExist) {
+      throw new Error('E-mail não cadastrado');
     }
+
+    const passwordMatched = compare(password, userExist.password);
+
+    if (!passwordMatched) {
+      throw new Error('Senha inválida');
+    }
+
+    const token = sign({ email: email }, process.env.JWT_SECRET!, {
+      subject: email,
+      expiresIn: '15min',
+    });
+
+    const refreshToken = sign({ email: email }, process.env.JWT_SECRET!, {
+      subject: email,
+      expiresIn: '7d',
+    });
+
+    return { token, refreshToken };
   }
 }
