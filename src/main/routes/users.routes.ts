67c7d7553707591a0b1d4578a0be5e5d141a88ec
ismael cezar <103972495+ismaelczar@users.noreply.github.com>
2025-05-31 13:41:26 -0300

    feat: add updated-password-controller in users.router

--- src/main/routes/users.routes.ts
+++ src/main/routes/users.routes.ts
@@ -3,19 +3,29 @@ import { CreateUserController } from '@modules/users/application/useCases/create
 import { ListUserController } from '@modules/users/application/useCases/listUsers/ListUsersController';
 import { RemoveUserController } from '@modules/users/application/useCases/removeUser/RemoveUserController';
 import { ensureAuthenticated } from 'main/http/middlewares/ensureAuthenticated';
+import { UpdatedPasswordUserController } from '@modules/users/application/useCases/updatedPasswordUser/UpdatedPasswordUserController';
 
 export const usersRouter = Router();
 
 const createUserController = new CreateUserController();
 const listUserController = new ListUserController();
 const removeUserController = new RemoveUserController();
+const updatedaPasswordController = new UpdatedPasswordUserController();
 
-usersRouter.post('/', createUserController.handle.bind(createUserController));
 usersRouter.get(
   '/',
   ensureAuthenticated,
   listUserController.handle.bind(listUserController),
 ); //TODO: Rota apenas para administradores
+
+usersRouter.post('/', createUserController.handle.bind(createUserController));
+
+usersRouter.patch(
+  '/',
+  ensureAuthenticated,
+  updatedaPasswordController.handle.bind(updatedaPasswordController),
+);
+
 usersRouter.delete(
   '/:id',
   ensureAuthenticated,
