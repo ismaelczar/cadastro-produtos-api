a53765a94d7ccc7034f76c2d7416a6bd8ce23b17
ismael cezar <103972495+ismaelczar@users.noreply.github.com>
2025-06-23 15:10:03 -0300

    feat: adding tests fot create and update procuct

---
+++ src/modules/products/application/useCases/createProduct/CreateProduct.spec.ts
@@ -0,0 +1,43 @@
+import { FakeProductRepository } from '@modules/products/domain/repositories/fakes/FakeProductRepository';
+import 'reflect-metadata';
+import { CreateProductUseCase } from './CreateProductUseCase';
+
+describe('CreateProduct', () => {
+  it('should be able to create a new product', async () => {
+    const fakeProductRepository = new FakeProductRepository();
+    const createProductUseCase = new CreateProductUseCase(
+      fakeProductRepository,
+    );
+
+    const product = {
+      name: 'Cadeira Gamer Ultimate',
+      price: 1299.9,
+      description: 'Conforto premium para longas sessões de jogo.',
+      long_description: [
+        'Encosto reclinável em até 180 graus.',
+        'Estofado de couro sintético com espuma de alta densidade.',
+        'Apoios de braço ajustáveis em 4 direções.',
+      ],
+      image_urls: [
+        'https://exemplo.com/imagens/cadeira1.jpg',
+        'https://exemplo.com/imagens/cadeira2.jpg',
+      ],
+      rating: 4.7,
+      reviewCount: 132,
+      features: [
+        'Ajuste de altura pneumático',
+        'Base reforçada de aço',
+        'Rodas silenciosas',
+      ],
+      isAvailable: true,
+      freeShipping: true,
+      shippingEstimate: '3 a 5 dias úteis',
+    };
+
+    const result = await createProductUseCase.execute(product);
+
+    expect(result).toHaveProperty('description');
+    expect(result.name).toBe(product.name);
+    expect(result.price).toBe(product.price);
+  });
+});
---
+++ src/modules/products/application/useCases/deleteProduct/DeleteProduct.spec.ts
@@ -0,0 +1,69 @@
+import 'reflect-metadata';
+import { FakeProductRepository } from '@modules/products/domain/repositories/fakes/FakeProductRepository';
+import { DeleteProductUseCase } from './DeleteProductUseCase';
+import { AppError } from '@shared/core/errors/AppError';
+
+describe('DeleteProduct', () => {
+  let fakeProductRepository: FakeProductRepository;
+  let deleteProductUseCase: DeleteProductUseCase;
+  let uuidFake: string;
+
+  beforeEach(() => {
+    fakeProductRepository = new FakeProductRepository();
+    deleteProductUseCase = new DeleteProductUseCase(fakeProductRepository);
+    uuidFake = 'df4e99f4-47e6-4649-9eae-a374f555f57f';
+  });
+
+  it('should be able to remove a product', async () => {
+    const fakeproductData = {
+      id: 'df4e99f4-47e6-4649-9eae-a374f555f57f',
+      name: 'Cadeira Gamer Ultimate',
+      price: 1299.9,
+      description: 'Conforto premium para longas sessões de jogo.',
+      long_description: [
+        'Encosto reclinável em até 180 graus.',
+        'Estofado de couro sintético com espuma de alta densidade.',
+        'Apoios de braço ajustáveis em 4 direções.',
+      ],
+      image_urls: [
+        'https://exemplo.com/imagens/cadeira1.jpg',
+        'https://exemplo.com/imagens/cadeira2.jpg',
+      ],
+      rating: 4.7,
+      reviewCount: 132,
+      features: [
+        'Ajuste de altura pneumático',
+        'Base reforçada de aço',
+        'Rodas silenciosas',
+      ],
+      isAvailable: true,
+      freeShipping: true,
+      shippingEstimate: '3 a 5 dias úteis',
+    };
+
+    const createProduct = await fakeProductRepository.create(fakeproductData);
+
+    await deleteProductUseCase.execute(createProduct.id);
+
+    const result = await fakeProductRepository.findById(createProduct.id);
+
+    expect(result).toBeNull();
+  });
+
+  it('should throw if ID is not a valid UUID', async () => {
+    try {
+      await deleteProductUseCase.execute('invalid-id');
+    } catch (error) {
+      expect(error).toBeInstanceOf(AppError);
+      expect(error.message).toBe('Invalid UUID');
+      expect(error.statusCode).toBe(400);
+      expect(error.type).toBe('validation');
+    }
+  });
+
+  it('should throw if ID is not a valid one', async () => {
+    await expect(deleteProductUseCase.execute(uuidFake)).rejects.toThrow(
+      'Provide a valid product',
+    );
+  });
+});
