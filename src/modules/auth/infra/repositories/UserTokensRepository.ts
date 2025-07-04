f1b033ab6045dd01ff3f982598090578e1250d57
ismael cezar <103972495+ismaelczar@users.noreply.github.com>
2025-07-03 23:14:08 -0300

    feat: add methods initial in tokens-repository

--- src/modules/auth/infra/repositories/UserTokensRepository.ts
+++ src/modules/auth/infra/repositories/UserTokensRepository.ts
@@ -1,12 +1,27 @@
 import { UserToken } from '@modules/auth/domain/entities/UserToken';
-import { IUserTokensRepository } from '@modules/users/domain/repositories/IUserTokensRepository';
+import { IUserTokensRepository } from '@modules/auth/domain/repositories/IUserTokensRepository';
+import { getRepository, Repository } from 'typeorm';
 
 export class UserTokensRepository implements IUserTokensRepository {
-  constructor() {}
+  ormRepo: Repository<UserToken>;
 
-  //TODO: deve criar um token com o id do usuário.
-  generate(user_id: string): Promise<UserToken> {}
+  constructor() {
+    this.ormRepo = getRepository(UserToken);
+  }
 
-  //TODO: Deve retornar o UserToken que tem o token fornecido.
-  findByToken(token: string): Promise<UserToken | undefined> {}
+  async generate(user_id: string): Promise<UserToken> {
+    const userToken = this.ormRepo.create({ user_id });
+
+    await this.ormRepo.save(userToken);
+
+    return userToken;
+  }
+
+  async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = await this.ormRepo.findOne({
+      where: { token },
+    });
+
+    return userToken;
+  }
 }
