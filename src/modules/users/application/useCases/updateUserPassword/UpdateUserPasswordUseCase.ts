dbd5d7e368d380b8d5a08930f3e6ac46ccc7a071
ismael cezar <103972495+ismaelczar@users.noreply.github.com>
2025-07-09 22:11:42 -0300

    feat: adding hashprovider for comparison and password generation in update-user-password

--- src/modules/users/application/useCases/updateUserPassword/UpdateUserPasswordUseCase.ts
+++ src/modules/users/application/useCases/updateUserPassword/UpdateUserPasswordUseCase.ts
@@ -1,14 +1,14 @@
-import { compare, hash } from 'bcrypt';
-
 import { User } from '@modules/users/domain/entities/User';
 import { IUserRepository } from '@modules/users/domain/repositories/IUserRepository';
 import { inject, injectable } from 'tsyringe';
 import { AppError } from '@shared/core/errors/AppError';
+import { IHashProvider } from '@shared/providers/hash/IHashProvider';
 
 @injectable()
 export class UpdateUserPasswordUseCase {
   constructor(
     @inject('UserRepository') private readonly usersRepository: IUserRepository,
+    @inject('HashProvider') private readonly hashProvider: IHashProvider,
   ) {}
 
   async execute(
@@ -19,16 +19,19 @@ export class UpdateUserPasswordUseCase {
     const user = await this.usersRepository.findByEmail(email);
 
     if (!user) {
-      throw new AppError('Combinação inválida', 409, 'validation');
+      throw new AppError('Usuário não encontrado', 409, 'validation');
     }
 
-    const passwordMatched = await compare(password, user.password);
+    const passwordMatched = await this.hashProvider.compareHash(
+      password,
+      user.password,
+    );
 
     if (!passwordMatched) {
-      throw new AppError('Combinação inválida', 409, 'validation');
+      throw new AppError('Senha inválida', 409, 'validation');
     }
 
-    user.password = await hash(newPassword, 8);
+    user.password = await this.hashProvider.generateHash(newPassword);
 
     const updatedUser = await this.usersRepository.updatePassword(user);
 
