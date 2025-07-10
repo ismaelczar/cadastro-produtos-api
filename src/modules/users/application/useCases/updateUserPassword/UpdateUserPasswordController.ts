966fdacb207aac09ee88b4fba04fc5afbe606c48
ismael cezar <103972495+ismaelczar@users.noreply.github.com>
2025-07-09 22:10:36 -0300

    refactor: setting the parameter name

--- src/modules/users/application/useCases/updateUserPassword/UpdateUserPasswordController.ts
+++ src/modules/users/application/useCases/updateUserPassword/UpdateUserPasswordController.ts
@@ -5,11 +5,11 @@ import { AppError } from '@shared/core/errors/AppError';
 
 export class UpdatedPasswordUserController {
   async handle(req: Request, res: Response): Promise<Response> {
-    const { emai, password, newPassword } = req.body;
+    const { email, password, newPassword } = req.body;
     const useCase = container.resolve(UpdateUserPasswordUseCase);
 
     try {
-      const result = await useCase.execute(emai, password, newPassword);
+      const result = await useCase.execute(email, password, newPassword);
       return res.json(result);
     } catch (err: unknown) {
       if (err instanceof AppError) {
