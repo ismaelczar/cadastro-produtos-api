879cbf458ecf62da0ec9eaa6fd195b492f1f092d
ismael cezar <103972495+ismaelczar@users.noreply.github.com>
2025-07-10 23:45:17 -0300

    feat: add express-validator for route validations

---
+++ src/main/http/middlewares/handleValidationErrors.ts
@@ -0,0 +1,11 @@
+import { Request, Response, NextFunction } from 'express'; // No need for RequestHandler here, as we're fixing the return type
+import { validationResult } from 'express-validator';
+
+export const validate = (req: Request, res: Response, next: NextFunction) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.status(400).json({ errors: errors.array() });
+    return;
+  }
+  next();
+};
--- src/main/routes/users.routes.ts
+++ src/main/routes/users.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import multer from 'multer';
+import { body } from 'express-validator';
 import { CreateUserController } from '@modules/users/application/useCases/createUser/CreateUserController';
 import { ListUserController } from '@modules/users/application/useCases/listUsers/ListUsersController';
 import { DeleteUserController } from '@modules/users/application/useCases/deleteUser/DeleteUserController';
@@ -8,6 +9,7 @@ import { UpdatedPasswordUserController } from '@modules/users/application/useCas
 import { UpdateUserAvatarController } from '@modules/users/application/useCases/updateUserAvatar/UpdateUserAvatarController';
 import uploadConfig from '@shared/config/upload';
 import { ensureAdmin } from 'main/http/middlewares/ensureAdmin';
+import { validate } from 'main/http/middlewares/handleValidationErrors';
 
 export const usersRouter = Router();
 const upload = multer(uploadConfig);
@@ -18,16 +20,51 @@ const deleteUserController = new DeleteUserController();
 const updateUserPasswordController = new UpdatedPasswordUserController();
 const updateUserAvatar = new UpdateUserAvatarController();
 
-usersRouter.post('/', createUserController.handle.bind(createUserController));
+usersRouter.post(
+  '/',
+  [
+    body('firstName')
+      .notEmpty()
+      .withMessage('O Nome é obrigatório')
+      .isLength({ min: 3 })
+      .withMessage('Deve ter no minimo 3 caracters'),
+    body('lastName')
+      .notEmpty()
+      .withMessage('O Sobrenome é obrigatório')
+      .isLength({ min: 3 })
+      .withMessage('Deve ter no minimo 3 caracters'),
+    body('email')
+      .notEmpty()
+      .withMessage('O Email é obrigatório')
+      .isEmail()
+      .withMessage('Deve ser fornecido um e-mail válido.'),
+    body('password').notEmpty().withMessage('A Senha é obrigatório'),
+
+    validate,
+  ],
+  createUserController.handle.bind(createUserController),
+);
 
 usersRouter.patch(
   '/me/password',
+  [
+    body('email')
+      .notEmpty()
+      .withMessage('O Email é obrigatório')
+      .isEmail()
+      .withMessage('Deve ser fornecido um e-mail válido.'),
+    body('password').notEmpty().withMessage('A senha é obrigatória.'),
+    body('newPassword').notEmpty().withMessage('A nova senha é obrigatória.'),
+
+    validate,
+  ],
   ensureAuthenticated,
   updateUserPasswordController.handle.bind(updateUserPasswordController),
 );
 
 usersRouter.patch(
   '/me/avatar',
+  [body('avatar').notEmpty().withMessage('Avatar é obrigatório.'), validate],
   ensureAuthenticated,
   upload.single('avatar'),
   updateUserAvatar.handle.bind(updateUserAvatar),
