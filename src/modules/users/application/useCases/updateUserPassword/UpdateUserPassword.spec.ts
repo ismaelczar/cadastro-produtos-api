5f81c29857a5261872a7c6a351501fc968a30318
ismael cezar <103972495+ismaelczar@users.noreply.github.com>
2025-07-13 09:31:55 -0300

    test: ensure password update use case behaves correctly with valid and invalid inputs

--- src/modules/users/application/useCases/updateUserPassword/UpdateUserPassword.spec.ts
+++ src/modules/users/application/useCases/updateUserPassword/UpdateUserPassword.spec.ts
@@ -1,22 +1,25 @@
 import 'reflect-metadata';
-import { hash } from 'bcrypt';
 import { FakeUserRepository } from '@modules/users/infra/fakes/FakeUserRepository';
 import { UpdateUserPasswordUseCase } from './UpdateUserPasswordUseCase';
 import { User } from '@modules/users/domain/entities/User';
 import { AppError } from '@shared/core/errors/AppError';
+import { FakeHashProvider } from '@modules/users/infra/fakes/FakeHashProvider';
 
 let fakeUserRepository: FakeUserRepository;
 let updatedPasswordUserUseCase: UpdateUserPasswordUseCase;
+let fakeHashProvider: FakeHashProvider;
 let fakeUser: User;
 
 describe('UpdatePasswordUser', () => {
   beforeEach(async () => {
     fakeUserRepository = new FakeUserRepository();
+    fakeHashProvider = new FakeHashProvider();
     updatedPasswordUserUseCase = new UpdateUserPasswordUseCase(
       fakeUserRepository,
+      fakeHashProvider,
     );
 
-    const hashedPassword = await hash('123456', 8);
+    const hashedPassword = await fakeHashProvider.generateHash('123456');
 
     fakeUser = {
       firstName: 'Teste',
@@ -28,15 +31,15 @@ describe('UpdatePasswordUser', () => {
     await fakeUserRepository.create(fakeUser);
   });
 
-  it('should be able to updated password user', async () => {
+  it('should be able to update user password', async () => {
     const result = await updatedPasswordUserUseCase.execute(
       'test@example.com',
       '123456',
-      '12345678',
+      'test123',
     );
 
     expect(result).toHaveProperty('email');
-    expect(result.password).toBeUndefined();
+    expect(result.password).not.toBe('123456');
   });
 
   it('should throw if user does not exist', async () => {
@@ -46,27 +49,18 @@ describe('UpdatePasswordUser', () => {
         '123456',
         'newpassword',
       ),
-    ).rejects.toEqual(new AppError('Combinação inválida', 409, 'validation'));
+    ).rejects.toEqual(
+      new AppError('Usuário não encontrado', 404, 'validation'),
+    );
   });
 
   it('should throw if password does not match', async () => {
-    const hashedPassword = await hash('123456', 8);
-
-    const user = {
-      firstName: 'Teste',
-      lastName: 'teste',
-      email: 'test@example.com',
-      password: hashedPassword,
-    };
-
-    await fakeUserRepository.create(user);
-
     await expect(
       updatedPasswordUserUseCase.execute(
         'test@example.com',
         'wrongpassword',
         'newpassword',
       ),
-    ).rejects.toEqual(new AppError('Combinação inválida', 409, 'validation'));
+    ).rejects.toEqual(new AppError('Senha inválida', 409, 'validation'));
   });
 });
